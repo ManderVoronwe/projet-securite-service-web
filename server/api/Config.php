197906527b23447a3c747e34cd1c8401f1a373b0
ManderVoronwe <95370877+ManderVoronwe@users.noreply.github.com>
2022-11-15 08:59:31 +0100

    i change my mind

---
+++ server/api/Config.php
@@ -0,0 +1,16 @@
+<?php
+    namespace server\api;
+    final class Config
+    {
+        
+        const 
+            DB_HOST = 'mymvcblog',
+            DB_NAME = 'mymvcblog',
+            DB_USR = 'root',
+            DB_PWD = 'pwd123',
+
+        // Title of the site
+        SITE_NAME = 'Maki restaurant';
+
+    }
+?>
